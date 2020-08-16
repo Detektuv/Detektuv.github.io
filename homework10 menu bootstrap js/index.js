c6a765e8c707b76847cd0d4c862532f05bc5d13c
Detektuv <mozhaeva10@ukr.net>
2020-08-16 23:26:50 +0300

    10 homework

---
+++ homework10 menu bootstrap js/index.js
@@ -0,0 +1,12 @@
+alert("Hello, Dima!!");
+// console.log("Hello world");
+
+let a = "hello";
+let b= "good morning";
+let c = "good day";
+let d = "respects";
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
